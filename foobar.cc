dee8a1b0e0d9e8d9e5a83f6d03dbdccd6a838698
RiJo <rajoo@mail.com>
2010-03-21 17:41:39 +0100

    added text-input for runfield in foobar

--- foobar.cc
+++ foobar.cc
@@ -1,22 +1,61 @@
 #include "glass.h"
 
-#define WORKSPACE_WIDTH     30
-#define WORKSPACES_WIDTH    ((WORKSPACE_WIDTH+ITEM_PADDING)*(workspace_count + 1))
-#define RUNFIELD_WIDTH      (5 * WORKSPACE_WIDTH)
-#define BAR_HEIGHT          20
-#define BAR_WIDTH           (((WORKSPACE_WIDTH+ITEM_PADDING)*(workspace_count + 1)) + RUNFIELD_WIDTH)
-#define ITEM_PADDING        3
-#define COLOR_ACTIVE        "#ff0000"
-#define COLOR_INACTIVE      "#555555"
-#define COLOR_TEXT          "#333333"
-
 FooBar::FooBar(Display *d, Window w, char &count, char &current) :
 workspace_count(count), current_workspace(current)
 {
     dpy = d;
     root = w;
 
+    setupColors();
+
+    // Run field
+    runfield_active = false;
+    runfield = (char *)malloc(RUNFIELD_BUFFER);
+    resetRunField();
+}
+
+FooBar::~FooBar()
+{
+    delete [] runfield;
+}
+
+void FooBar::redrawWorkspaces()
+{
+    XTextItem *text = new XTextItem();
+    text->chars = (char *)malloc(1);
+    text->nchars = 1;
+
+    int x, y;
+    for (char ws = 0; ws <= workspace_count; ws++) {
+        x = 1+((WORKSPACE_WIDTH+ITEM_PADDING)*ws);
+        y = 1;
+        
+        XFillRectangle(dpy, root, background_gc, x, y, WORKSPACE_WIDTH, BAR_HEIGHT);
+        if (ws == current_workspace) {
+            XDrawRectangle(dpy, root, active_gc, x, y, WORKSPACE_WIDTH, BAR_HEIGHT);
+        }
+        else {
+            XDrawRectangle(dpy, root, inactive_gc, x, y, WORKSPACE_WIDTH, BAR_HEIGHT);
+        }
+
+        if (ws > 0) {
+            text->chars[0] = (char)(48 + ws);
+            XDrawText(dpy, root, text_gc, x + MAGIC_NUMBER - 1, y + MAGIC_NUMBER, text, 1);
+        }
+    }
+
+    delete [] text->chars;
+    delete [] text;
+}
+
+void FooBar::setupColors() {
     colormap = DefaultColormap(dpy, 0);
+
+    XColor background_color;
+    background_gc = XCreateGC(dpy, root, 0, 0);
+    XParseColor(dpy, colormap, COLOR_BACKGROUND, &background_color);
+    XAllocColor(dpy, colormap, &background_color);
+    XSetForeground(dpy, background_gc, background_color.pixel);
 
     XColor active_color;
     active_gc = XCreateGC(dpy, root, 0, 0);
@@ -35,34 +74,6 @@ workspace_count(count), current_workspace(current)
     XParseColor(dpy, colormap, COLOR_TEXT, &text_color);
     XAllocColor(dpy, colormap, &text_color);
     XSetForeground(dpy, text_gc, text_color.pixel);
-
-    // Run field
-    runfield_active = false;
-}
-
-void FooBar::redrawWorkspaces()
-{
-    XTextItem *text = new XTextItem();
-    text->chars = (char *)malloc(1);
-    text->nchars = 1;
-
-    int x, y;
-    for (char ws = 0; ws <= workspace_count; ws++) {
-        x = 1+((WORKSPACE_WIDTH+ITEM_PADDING)*ws);
-        y = 1;
-        if (ws == current_workspace)
-            XDrawRectangle(dpy, root, active_gc, x, y, WORKSPACE_WIDTH, BAR_HEIGHT);
-        else
-            XDrawRectangle(dpy, root, inactive_gc, x, y, WORKSPACE_WIDTH, BAR_HEIGHT);
-
-        if (ws > 0) {
-            text->chars[0] = (char)(48 + ws);
-            XDrawText(dpy, root, text_gc, x + (WORKSPACE_WIDTH/2) - 1, y + (WORKSPACE_WIDTH/2), text, 1);
-        }
-    }
-
-    delete [] text->chars;
-    delete [] text;
 }
 
 void FooBar::redrawRunField()
@@ -73,11 +84,19 @@ void FooBar::redrawRunField()
     else
         gc = inactive_gc;
 
-    // temp
-    int x = ((WORKSPACE_WIDTH+ITEM_PADDING)*(workspace_count + 1));
-    // temp
+    int x = WORKSPACES_WIDTH;
     int y = 1;
+    
+    XFillRectangle(dpy, root, background_gc, x, y, RUNFIELD_WIDTH, BAR_HEIGHT);
     XDrawRectangle(dpy, root, gc, x, y, RUNFIELD_WIDTH, BAR_HEIGHT);
+
+    XTextItem *text = new XTextItem();
+    text->chars = runfield;
+    text->nchars = strlen(runfield);
+
+    XDrawText(dpy, root, text_gc, x + MAGIC_NUMBER, y + MAGIC_NUMBER, text, 1);
+
+    delete [] text;
 }
 
 void FooBar::redraw()
@@ -106,15 +125,41 @@ inline bool FooBar::insideRunfield(int x, int y)
     );
 }
 
+void FooBar::resetRunField()
+{
+    current_character = 0;
+    for (int i = 0; i < RUNFIELD_BUFFER; i++) {
+        runfield[i] = '\0';
+    }
+}
+
 void FooBar::handleButtonEvent(XButtonEvent *e)
 {
     //printf("Handle button:\n\tinsideRunfield() == %d\n\tinsideWorkspace() == %d\n", insideRunfield(e->x, e->y), insideWorkspaces(e->x, e->y));
     switch (e->button) {
         case Button1:
             runfield_active = insideRunfield(e->x, e->y);
+
             if (insideWorkspaces(e->x, e->y)) {
                 wm->goToWorkspace(e->x / (WORKSPACE_WIDTH+ITEM_PADDING));
             }
+        break;
+    }
+}
+
+void FooBar::handleKeyEvent(XKeyEvent *e)
+{
+    KeySym ks=XKeycodeToKeysym(dpy, e->keycode, 0);
+    if (ks == NoSymbol)
+        return;
+    //printf("handle key: %d\t keysym: %ld\tstring: %s\n", e->keycode, (long)ks, XKeysymToString(ks));
+    switch (ks) {
+        case XK_Return:
+            forkExec(runfield);
+            resetRunField();
+        break;
+        default:
+            runfield[current_character++] = *XKeysymToString(ks);
         break;
     }
 }
--- foobar.h
+++ foobar.h
@@ -3,6 +3,21 @@
 
 #include "glass.h"
 
+#define WORKSPACE_WIDTH     30
+#define WORKSPACES_WIDTH    ((WORKSPACE_WIDTH+ITEM_PADDING)*(workspace_count + 1))
+#define RUNFIELD_WIDTH      (5 * WORKSPACE_WIDTH)
+#define BAR_HEIGHT          20
+#define BAR_WIDTH           (((WORKSPACE_WIDTH+ITEM_PADDING)*(workspace_count + 1)) + RUNFIELD_WIDTH)
+#define ITEM_PADDING        3
+#define COLOR_BACKGROUND    "#050505"
+#define COLOR_ACTIVE        "#ff0000"
+#define COLOR_INACTIVE      "#555555"
+#define COLOR_TEXT          "#333333"
+
+#define RUNFIELD_BUFFER     255
+
+#define MAGIC_NUMBER 15
+
 class FooBar
 {
 private: /* Member Variables */
@@ -10,6 +25,7 @@ private: /* Member Variables */
     Window root;
 
     Colormap colormap;
+    GC background_gc;
     GC active_gc;
     GC inactive_gc;
     GC text_gc;
@@ -19,8 +35,11 @@ private: /* Member Variables */
     char &current_workspace;
     // Run field
     bool runfield_active;
+    char *runfield;
+    unsigned int current_character;
 
 private:
+    void setupColors();
     void redrawWorkspaces();
     void redrawRunField();
 
@@ -29,10 +48,16 @@ private:
 
 public: /* Member Functions */
     FooBar(Display *, Window, char &, char &);
+    ~FooBar();
+
     void redraw();
 
     void setRunfield(bool active) { runfield_active = active; }
+    bool getRunfield() { return runfield_active; }
+    void resetRunField();
+
     void handleButtonEvent(XButtonEvent *);
+    void handleKeyEvent(XKeyEvent *);
 };
 
 
